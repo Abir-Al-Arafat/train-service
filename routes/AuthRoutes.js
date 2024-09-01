72c2106f47b4de466da492aa6dc7f8eede66fbae
Abir-Al-Arafat <antorabir@gmail.com>
2024-09-01 14:41:09 +0600

    authentication added

---
+++ routes/AuthRoutes.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const routes = express();
+const AuthController = require("../controller/AuthController");
+const { userValidator, authValidator } = require("../middleware/validation");
+// const { authValidator } = require("../middleware/authValidation");
+
+// for signing up
+routes.post(
+  "/auth/signup",
+  //   userValidator.create,
+  //   authValidator.create,
+  AuthController.signup
+);
+
+// for logging in
+routes.post(
+  "/auth/login",
+  // authValidator.login,
+  AuthController.login
+);
+
+// for logging in
+// routes.post('/auth/login', authValidator.login, ProductController.getAll)
+
+module.exports = routes;
